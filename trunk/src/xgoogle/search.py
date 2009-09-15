99a6fc64aa3661bc365139a61bcdbce7731d5e88
iman.karim@smail.inf.fh-bonn-rhein-sieg.de <iman.karim@smail.inf.fh-bonn-rhein-sieg.de@73730ac2-96f3-11de-85ac-4b769f26a8fe>
2009-09-15 10:04:33 +0000

    Removed GET parameter in google module which preferes english results (thanks exo for this hint)

--- trunk/src/xgoogle/search.py
+++ trunk/src/xgoogle/search.py
@@ -49,10 +49,10 @@ class SearchResult:
         return 'Google Search Result: "%s"' % self.title
 
 class GoogleSearch(object):
-    SEARCH_URL_0 = "http://www.google.com/search?hl=en&q=%(query)s&btnG=Google+Search"
-    NEXT_PAGE_0 = "http://www.google.com/search?hl=en&q=%(query)s&start=%(start)d"
-    SEARCH_URL_1 = "http://www.google.com/search?hl=en&q=%(query)s&num=%(num)d&btnG=Google+Search"
-    NEXT_PAGE_1 = "http://www.google.com/search?hl=en&q=%(query)s&num=%(num)d&start=%(start)d"
+    SEARCH_URL_0 = "http://www.google.com/search?q=%(query)s&btnG=Google+Search"
+    NEXT_PAGE_0 = "http://www.google.com/search?q=%(query)s&start=%(start)d"
+    SEARCH_URL_1 = "http://www.google.com/search?q=%(query)s&num=%(num)d&btnG=Google+Search"
+    NEXT_PAGE_1 = "http://www.google.com/search?q=%(query)s&num=%(num)d&start=%(start)d"
 
     def __init__(self, query, random_agent=False, debug=False):
         self.query = query
