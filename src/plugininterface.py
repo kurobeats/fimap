ebe8096b78e569e7c5191f872390fc4dbd14de03
fimap.dev <fimap.dev@73730ac2-96f3-11de-85ac-4b769f26a8fe>
2010-02-21 18:58:51 +0000

    License header added to plugininterface.py

--- src/plugininterface.py
+++ src/plugininterface.py
@@ -1,3 +1,23 @@
+#
+# This file is part of fimap.
+#
+# Copyright(c) 2009-2010 Iman Karim(ikarim2s@smail.inf.fh-brs.de).
+# http://fimap.googlecode.com
+#
+# This file may be licensed under the terms of of the
+# GNU General Public License Version 2 (the ``GPL'').
+#
+# Software distributed under the License is distributed
+# on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+# express or implied. See the GPL for the specific language
+# governing rights and limitations.
+#
+# You should have received a copy of the GPL along with this
+# program. If not, go to http://www.gnu.org/licenses/gpl.html
+# or write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+
 from baseClass import baseClass
 import os, sys
 
